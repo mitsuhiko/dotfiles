1ed34db431a1841ed8c1aa69ba90ddf5431c1123
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-03 12:22:11 +0200

    Improved workenv virtualenv support

--- zsh/custom/plugins/workenv/workenv.plugin.zsh
+++ zsh/custom/plugins/workenv/workenv.plugin.zsh
@@ -2,13 +2,9 @@ if [[ -z $WORKENV_AUTH_FILE ]]; then
   WORKENV_AUTH_FILE=~/.workenv_authorized
 fi
 
-if [[ -z $WORKENV_ENTER ]]; then
-  WORKENV_ENTER=".workenv"
-fi
-
-if [[ -z $WORKENV_LEAVE ]]; then
-  WORKENV_LEAVE=".workenv-leave"
-fi
+# Allow disabling the workenv logic.  This is done for shell scripts
+# that want to cd around and can cause confusion.
+WORKENV_ENABLED=1
 
 function workenv_check_and_run() {
   echo "$fg_no_bold[red]WARNING$reset_color"
@@ -45,12 +41,15 @@ function workenv_check_and_exec() {
 }
 
 function workenv_init() {
+  if [[ ! "$WORKENV_ENABLED" == "1" ]]; then
+    return
+  fi
   _WORKENV_OLDPATH="$OLDPWD"
   _WORKENV_NEWPATH="$(pwd)"
 
   while [[ ! "$_WORKENV_NEWPATH" == "$_WORKENV_OLDPATH"* ]]; do
-    if [[ -f "$_WORKENV_OLDPATH/$WORKENV_LEAVE" ]]; then
-      workenv_check_and_exec "$_WORKENV_OLDPATH/$WORKENV_LEAVE"
+    if [[ -f "$_WORKENV_OLDPATH/.workenv-leave" ]]; then
+      workenv_check_and_exec "$_WORKENV_OLDPATH/.workenv-leave"
     fi
     _WORKENV_OLDPATH="$(dirname $_WORKENV_OLDPATH)"
   done
@@ -61,14 +60,14 @@ function workenv_init() {
 
   while [[ ! "$_WORKENV_OLDPATH" == "$_WORKENV_NEWPATH" ]]; do
     _WORKENV_OLDPATH="$_WORKENV_OLDPATH$(echo -n '/'; echo ${_WORKENV_NEWPATH#${_WORKENV_OLDPATH}} | tr \/ "\n" | sed -n '2p' )"
-    if [[ -f "$_WORKENV_OLDPATH/$WORKENV_ENTER" ]]; then
-      workenv_check_and_exec "$_WORKENV_OLDPATH/$WORKENV_ENTER"
+    if [[ -f "$_WORKENV_OLDPATH/.workenv" ]]; then
+      workenv_check_and_exec "$_WORKENV_OLDPATH/.workenv"
     fi
   done
 }
 
-if [[ -f "./$WORKENV_ENTER" ]]; then
-  workenv_check_and_exec "./$WORKENV_ENTER"
+if [[ -f "./.workenv" ]]; then
+  workenv_check_and_exec "./.workenv"
 fi
 
 () {
@@ -76,3 +75,48 @@ fi
   workenv_init
 }
 chpwd_functions+=( workenv_init )
+
+
+# Helpers that make working with workenv more fun
+
+_WORKENV_ACTIVE_VIRTUALENVS=()
+
+function workenv_enable_virtualenv() {
+  WORKENV_ENABLED=0
+  local OLD_ENV=$_WORKENV_ACTIVE_VIRTUALENVS[-1]
+  if [[ ! -z $OLD_ENV ]]; then
+    _workenv_disable_virtualenv
+  fi
+  _WORKENV_ACTIVE_VIRTUALENVS+=($1)
+  _workenv_enable_virtualenv "$1"
+  WORKENV_ENABLED=1
+}
+
+function workenv_disable_virtualenv() {
+  WORKENV_ENABLED=0
+  _workenv_disable_virtualenv
+  local OLD_ENV=$_WORKENV_ACTIVE_VIRTUALENVS[-2]
+  _WORKENV_ACTIVE_VIRTUALENVS=($_WORKENV_ACTIVE_VIRTUALENVS[1,-2])
+  if [[ ! -z $OLD_ENV ]]; then
+    _workenv_enable_virtualenv "$OLD_ENV"
+  fi
+  WORKENV_ENABLED=1
+}
+
+
+function _workenv_enable_virtualenv() {
+  echo -n "Enabling virtualenv $1 ... "
+  if [[ "${1#*:/}" != "$1" ]]; then
+    source "$1/bin/activate"
+  else
+    workon $1
+  fi
+  echo -e "$fg_no_bold[green]Done$reset_color"
+}
+
+function _workenv_disable_virtualenv() {
+  local ENV_NAME=$_WORKENV_ACTIVE_VIRTUALENVS[-1]
+  echo -n "Disabling virtualenv $ENV_NAME ... "
+  deactivate 2> /dev/null
+  echo -e "$fg_no_bold[green]Done$reset_color"
+}
