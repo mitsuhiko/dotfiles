00155c9a747d19c93521239a0b4388de6dfb0cbb
Armin Ronacher <armin.ronacher@active-4.com>
2020-02-17 23:06:29 +0100

    Some more changes

--- python/pythonstartup.py
+++ python/pythonstartup.py
@@ -11,18 +11,25 @@ def _init():
     import uuid
     import pprint
     import hashlib
+    import subprocess
     import datetime
     try:
         import __builtin__
     except ImportError:
         import builtins as __builtin__
 
+    PY2 = sys.version_info[0] == 2
     __import__('rlcompleter')
     histdir = os.path.expanduser('~/.pyhist')
     try:
         os.makedirs(histdir)
     except OSError:
         pass
+
+    if PY2:
+        text_type = unicode
+    else:
+        text_type = str
 
     def _b(x):
         if not isinstance(x, bytes):
@@ -56,6 +63,34 @@ def _init():
         return uuid.UUID(val)
 
 
+    def _dump_json(x, as_string=False, indent=2, cp=False):
+        import json
+        s = '\n'.join(x.rstrip() for x in json.dumps(x, indent=indent).rstrip().splitlines())
+        if cp:
+            _copy(s)
+        if as_string:
+            return s
+        print(s)
+
+    def _cat(path):
+        with open(path, 'rb') as f:
+            return f.read()
+
+    def _tcat(path):
+        return _cat(path).decode('utf-8')
+
+    def _paste():
+        return subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE).communicate()[0]
+
+    def _tpaste():
+        return _paste().decode('utf-8')
+
+    def _copy(val):
+        if isinstance(val, text_type):
+            val = val.encode('utf-8')
+        return subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE).communicate(val)
+
+
     helpers = types.ModuleType('helpers')
     helpers.histfile = histfile
     helpers.pp = pprint.pprint
@@ -69,7 +104,17 @@ def _init():
     helpers.td = datetime.timedelta
     helpers.timedelta = datetime.timedelta
     helpers.time = time.time
+    helpers.j = _dump_json
+    helpers.cat = _cat
+    helpers.tcat = _tcat
+    helpers.cp = _copy
+    helpers.copy = _copy
+    helpers.paste = _paste
+    helpers.tpaste = _tpaste
     __builtin__.h = helpers
+    __builtin__.true = True
+    __builtin__.false = False
+    __builtin__.null = None
 
 
 _init()
