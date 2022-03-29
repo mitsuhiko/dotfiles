bd21289b5d217d212b160d548fcba350274831e0
Armin Ronacher <armin.ronacher@active-4.com>
2022-03-29 21:41:39 +0200

    Switch to Python 3 for prompt

--- zsh/custom/plugins/transportstatus/transportstatus.plugin.zsh
+++ zsh/custom/plugins/transportstatus/transportstatus.plugin.zsh
@@ -1,3 +1,3 @@
 function transportstatus_prompt_info() {
-  PLANE_TEMPLATE="$ZSH_THEME_PLANEINFO_TEMPLATE" TRAIN_TEMPLATE="$ZSH_THEME_TRAININFO_TEMPLATE" python "$ZSH_CUSTOM/plugins/transportstatus/transportstatus.py"
+  PLANE_TEMPLATE="$ZSH_THEME_PLANEINFO_TEMPLATE" TRAIN_TEMPLATE="$ZSH_THEME_TRAININFO_TEMPLATE" python3 "$ZSH_CUSTOM/plugins/transportstatus/transportstatus.py"
 }
